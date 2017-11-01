793a53ff0ac4649a659f1076d398197a6ce459a8
Tirol_JPN <Tirol.JPN@gmail.com>
2017-11-01 10:09:59 +0900

    3章まで終了

--- src/lifegame/BoardModel.java
+++ src/lifegame/BoardModel.java
@@ -3,10 +3,12 @@ package lifegame;
 public class BoardModel {
 	private int cols;
 	private int rows;
+	private boolean[][] cells;
 
 	public BoardModel(int c, int r) {
 		cols = c;
 		rows = r;
+		cells = new boolean[rows][cols];
 	}
 
 	public int getCols() {
@@ -16,7 +18,22 @@ public class BoardModel {
 	public int getRows() {
 		return rows;
 	}
-	
-	
+
+	public void printForDebug() {
+		for(int y=0; y < rows; y++) {
+			for(int x=0; x < cols; x++) {
+				if(cells[y][x] == true) System.out.print('*');
+				else System.out.print('.');
+			}
+			System.out.println("");
+		}
+	}
+
+	public void changeCellState(int x, int y) {
+		// (x, y) で指定されたセルの状態を変更する．
+		cells[y][x]=!cells[y][x];
+	}
+
+
 
 }
